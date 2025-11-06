5e3af6913937c49ee4df0d9a3dc4ee046789bc6b
Lacoque <pamela.lacoquex@gmail.com>
2025-11-06 17:04:32 -0300

    cors

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const PORT = process.env.PORT || 3000;
 const corsOptions = {
   origin: [
     'https://formulario-contenidx.onrender.com',
-    'https://admin-contenidx.onrender.com'
+    'https://admin-contenidx.onrender.com',
+    'https://formulario-dinamico-front.onrender.com'
   ],
   credentials: true,
   optionsSuccessStatus: 200
